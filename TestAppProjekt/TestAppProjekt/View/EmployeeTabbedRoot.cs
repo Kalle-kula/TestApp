0cc9b79e54e6f9979fe8eaf1e4d9e457e09270d7
Kalle <Kalle.hallert@gmail.com>
2016-01-21 09:54:31 +0100

    Push for Lina

--- TestAppProjekt/TestAppProjekt/View/EmployeeTabbedRoot.cs
+++ TestAppProjekt/TestAppProjekt/View/EmployeeTabbedRoot.cs
@@ -3,42 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 using Xamarin.Forms;
 
 namespace TestAppProjekt.View
 {
-    public class TabsPage : TabbedPage
+    public class EmployeeTabbedRoot : TabbedPage
     {
-        public TabsPage()
+        public EmployeeTabbedRoot()
         {
             this.Children.Add(
                 new NavigationPage(new EmployeesPage())
-            {
-                Title="Employees"
-            });
+                {
+                    Title = "Employees"
+                });
 
             this.Children.Add(
                 new NavigationPage(new HomeScreen())
                 {
                     Title = "Home"
                 });
-
-
-            //Skapa ny class som inte har xaml och som ser ut som den här och sen länkar till sidorna som ska komma upp som tabbade
         }
     }
 }
-
-
-
-
-
-//public partial class EmployeeView : ContentPage
-//    {
-//        public EmployeeView()
-//        {
-//            InitializeComponent();
-//        }
-//    }
-
